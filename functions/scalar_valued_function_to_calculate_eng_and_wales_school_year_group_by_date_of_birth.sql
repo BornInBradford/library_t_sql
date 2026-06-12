a28cb2a3a66b1f2a41ab52d8941e0bc264cf53f6
BTHT\newshama <alex.newsham@bthft.nhs.uk>
2026-06-12 17:13:54 +0100

    initial commit for a function that takes a date of birth as a parameter and caluculates the likely school year group. If the function registers that the date of birth is in-eligible for a school year group, i.e. that the birth date equates to a year group value that is out of the scope of the range of Reception Year to Year 13 (i.e. last year of 6th form), then the result confirms that the data of birth is either pre-school or for an individual who is of post-school age.

---
+++ functions/scalar_valued_function_to_calculate_eng_and_wales_school_year_group_by_date_of_birth.sql
@@ -0,0 +1,46 @@
+use Util
+go
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- ====================================================================================================================================================================================
+-- Created by: Alex Newsham
+-- Create on: 12/06/2026
+-- Purpose:	To calculate the academic year group for a given data of birth on the basis of the current year, where the age is disqualified as 
+--          pre-school or post-school where out of scope i.e. <4 or >18 years of age
+-- ====================================================================================================================================================================================
+CREATE FUNCTION dbo.get_school_year_group
+(
+	-- function parameter(s)
+	@DoB date = null
+)
+RETURNS varchar(50)
+AS
+BEGIN
+	-- Declare internal variables
+	DECLARE @curYear char(4), @YearGroup int, @YearGroupResult varchar(50)
+
+	set @curYear = year(getdate())
+
+	-- Add the T-SQL statements to compute the return value here
+set @YearGroup = (select case when MONTH(@DoB) <= 8 then @curYear - YEAR(@DoB) -5
+     	                      else @curYear - YEAR(@DoB) -6
+				          end 
+				  )
+
+set @YearGroupResult = cast((select case when @YearGroup <= -1 then 'Pre-School age'
+									when @YearGroup > 13 then 'Post-school age'
+									when @YearGroup = 0 then 'Reception'
+									else cast(@YearGroup as varchar(20))
+									end)
+						     as varchar(20))
+
+
+	-- Return the result of the function
+	RETURN @YearGroupResult
+
+END
+GO
+
